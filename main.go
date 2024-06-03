60f1a9ead90703879105c09f6936f5974e343da5
WangZhenghai <fisschl@163.com>
2024-06-03 16:52:09 +0800

    添加路由

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/fisschl/fiber/emqx"
 	"github.com/fisschl/fiber/oss"
 	"github.com/fisschl/fiber/utils"
 	"log"
@@ -9,5 +10,6 @@ import (
 func main() {
 	app := utils.NewFiberApp()
 	oss.RegisterRouter(app)
+	emqx.RegisterRouter(app)
 	log.Fatal(app.Listen(":648"))
 }
